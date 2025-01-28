a54019f400dfb725d392cae5f61734f1c9530105
abdulhayevbahrom <abdulhayevbahromjon2002@gmail.com>
2025-01-28 15:40:53 +0500

    start

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const Api = axios.create({
   baseURL: "http://localhost:8080/api",
+  // baseURL: "https://edify-server-beta.vercel.app/api",
 });
 
 export default Api;
