6b64b51a75340ed303eba0e34bd86e65b261a47f
Asqarjon10dev <asqarjonumarxonov1010@gmail.com>
2025-02-04 14:15:07 +0500

    profile

---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from 'react';
+import "./Profile.css"
+import { Link } from 'react-router-dom';
+import { FaTelegram, FaInstagram, FaFacebook, FaTiktok, FaYoutube } from "react-icons/fa";
+import axios from '../../api';
+
+
+function Profile({ handleProfileClick }) {
+  const [data, setData] = useState([]);
+  const teacherName = "";
+  const teacher_result = getInitials(teacherName);
+
+  useEffect(() => {
+    axios
+      .get("/teacher/all", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((res) => {
+        setData(res.data.innerData);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  function getInitials(name) {
+    const words = name.split(" ");
+    return words.map((word) => word.charAt(0).toUpperCase()).join("");
+  }
+
+  return (
+    <div className='Profile'>
+      <div className="Profile_box">
+        {data.map((item, index) => {
+          <div className="user_img">
+            <img src="" alt="" />
+            <h2>{item.fullname}</h2>
+            <p>{item.specialty}</p>
+            <p>{item.address}</p>
+            <button>Xabar jo'natish</button>
+          </div>
+        })}
+
+        {data.map((item, index) => {
+          <div className="user_about">
+            <table>
+              <tbody>
+                <tr>
+                  <td>Ism familyasi</td>
+                  <td className='Main_td'>{item.fullname}</td>
+                </tr>
+                <tr>
+                  <td>mutaxasisligi</td>
+                  <td className='Main_td'>{item.specialty}</td>
+                </tr>
+                <tr>
+                  <td>Tel raqami</td>
+                  <td className='Main_td'>{item.phone}</td>
+                </tr>
+                <tr>
+                  <td>Manzili</td>
+                  <td className='Main_td'>{item.address}</td>
+                </tr>
+                <tr>
+                  <button onClick={() => handleProfileClick(console.log("ok"))} className='Edit_btn'>taxrirlash</button>
+                </tr>
+              </tbody>
+            </table>
+
+            <div className="Profile_social-media">
+              <Link to="telegram"><FaTelegram />  Telegram</Link>
+              <Link to="instagram"><FaInstagram /> Instagram</Link>
+              <Link to="facebook"><FaFacebook /> Facebook</Link>
+              <Link to="tik tok"><FaTiktok /> Tiktok</Link>
+              <Link to="youtobe"><FaYoutube /> Youtobe</Link>
+            </div>
+          </div>
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Profile;
--- src/router/login/Login.jsx
+++ src/router/login/Login.jsx
@@ -1,8 +1,66 @@
+// import React from "react";
+// import "./Login.css";
+// import { Button, Form, message } from "antd";
+// import axios from "../../api";
+// import { useNavigate } from "react-router-dom";
+// import { FaRegCircleUser } from "react-icons/fa6";
+// import { TbLockPassword } from "react-icons/tb";
+
+// function Login() {
+//   const [form] = Form.useForm();
+//   const navigate = useNavigate();
+
+//   const signIn = () => {
+//     let data = form.getFieldsValue();
+//     console.log(data)
+//     axios
+//       .post("/edu/login", data)
+//       .then((res) => {
+//         if (res.data.state) {
+//           localStorage.setItem("token", res.data.innerData.token);
+//           navigate("/");
+//         }
+//       })
+//       .catch((err) => {
+//         message.error(err.response.data.message);
+//         console.log(err);
+//       });
+//   };
+
+//   return (
+//     <div className="login">
+//       <Form form={form}  className="login-form">
+//         <h1>Login</h1>
+//         <Form.Item
+//           name="login"
+//           rules={[{ required: true, message: "Please input your username!" }]}
+//         >
+//           <FaRegCircleUser /> 
+//           <input type="text" placeholder="Username" />
+//         </Form.Item>
+//         <Form.Item
+//           name="password"
+//           rules={[{ required: true, message: "Please input your password!" }]}
+//         >
+//           <TbLockPassword />
+//           <input type="password" placeholder="Password" />
+//         </Form.Item>
+//         <Button onClick={signIn}>Login</Button>
+//       </Form>
+//     </div>
+//   );
+// }
+
+// export default Login;
+
+
 import React from "react";
 import "./Login.css";
 import { Button, Form, message } from "antd";
 import axios from "../../api";
 import { useNavigate } from "react-router-dom";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { TbLockPassword } from "react-icons/tb";
 
 function Login() {
   const [form] = Form.useForm();
@@ -15,6 +73,7 @@ function Login() {
       .then((res) => {
         if (res.data.state) {
           localStorage.setItem("token", res.data.innerData.token);
+          localStorage.setItem("user", res.data.innerData.user);
           navigate("/");
         }
       })
@@ -32,12 +91,14 @@ function Login() {
           name="login"
           rules={[{ required: true, message: "Please input your username!" }]}
         >
+          <FaRegCircleUser /> 
           <input type="text" placeholder="Username" />
         </Form.Item>
         <Form.Item
           name="password"
           rules={[{ required: true, message: "Please input your password!" }]}
         >
+          <TbLockPassword />
           <input type="password" placeholder="Password" />
         </Form.Item>
         <Button onClick={signIn}>Login</Button>
